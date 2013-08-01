c2f1081b2ed957da29e44cc8bcbb02f7ffbef9c6
Boris Zentner <bzentner@maxmind.com>
2013-08-01 12:57:02 +0200

    The example test.py should work with py2 and 3

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import MMDB
-print MMDB.lib_version();
-mmdb =MMDB.new("/usr/local/share/GeoIP2/GeoIP2-City.mmdb",MMDB.MMDB_MODE_MEMORY_CACHE )
+print (MMDB.lib_version());
+mmdb = MMDB.new("/usr/local/share/GeoIP2/GeoIP2-City.mmdb",MMDB.MMDB_MODE_MEMORY_CACHE )
 
-print mmdb.lookup("24.24.24.24")
+print (mmdb.lookup("24.24.24.24"))
 
