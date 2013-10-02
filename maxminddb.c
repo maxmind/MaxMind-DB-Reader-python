442bb7848f6d3ad5fdf4d137485d04f1b3632d3b
Boris Zentner <bzentner@maxmind.com>
2013-10-02 19:17:31 +0200

    Fix MOD_INIT return type for pythn 2 & 3

--- maxminddb.c
+++ maxminddb.c
@@ -484,7 +484,11 @@ MOD_INIT(maxminddb){
 #endif
 
     if (!m) {
+#if PY_MAJOR_VERSION >= 3
         return NULL;
+#else
+        return;
+#endif
     }
 
     init_type(m, &Reader_Type);
@@ -493,7 +497,11 @@ MOD_INIT(maxminddb){
     MaxMindDB_error = PyErr_NewException("maxminddb.InvalidDatabaseError", NULL,
                                          NULL);
     if (MaxMindDB_error == NULL) {
+#if PY_MAJOR_VERSION >= 3
         return NULL;
+#else
+        return;
+#endif
     }
     Py_INCREF(MaxMindDB_error);
     PyModule_AddObject(m, "InvalidDatabaseError", MaxMindDB_error);
