27d1431bb8dc0b19bd3bfc12ac9566c659f57ebd
Gregory Oschwald <goschwald@maxmind.com>
2013-10-15 10:48:38 -0700

    Bumped version

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ module = Extension(
 
 setup(
     name='maxminddb',
-    version='0.1.1',
+    version='0.2.0',
     description='Python extension for reading the MaxMind DB format',
     ext_modules=[module],
     long_description=open('README.rst').read(),
