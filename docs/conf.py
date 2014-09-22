406b8bf78dc0125544c9631d3e0421f4e8d8ed4e
Gregory Oschwald <goschwald@maxmind.com>
2014-09-22 13:19:36 -0700

    Get doc version from module

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,11 @@
 import sys
 import os
 
-__version__ = '0.2.1'
+sys.path.insert(0, os.path.abspath('..'))
+import maxminddb
+
+
+__version__ = maxminddb.__version__
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
