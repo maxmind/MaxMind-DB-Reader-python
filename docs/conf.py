5b9543e049073a68384fa47828699b7afddefba0
Gregory Oschwald <goschwald@maxmind.com>
2021-09-18 09:58:04 -0700

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "maxminddb"
-copyright = "2013-2020, MaxMind, Inc."
+copyright = "2013-2021, MaxMind, Inc."
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
