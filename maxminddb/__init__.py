cbc7e6ce63544aee3a655ba8819462b2fb197f97
Gregory Oschwald <goschwald@maxmind.com>
2025-01-09 07:45:09 -0800

    Update for v2.6.3

--- maxminddb/__init__.py
+++ maxminddb/__init__.py
@@ -81,7 +81,7 @@ def open_database(
 
 
 __title__ = "maxminddb"
-__version__ = "2.6.2"
+__version__ = "2.6.3"
 __author__ = "Gregory Oschwald"
 __license__ = "Apache License, Version 2.0"
 __copyright__ = "Copyright 2013-2025 MaxMind, Inc."
