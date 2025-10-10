a8a403c4efd7b00483a86c0449bb6e90743606dc
Gregory Oschwald <goschwald@maxmind.com>
2025-10-10 08:43:45 -0700

    Use modern union syntax throughout

--- maxminddb/types.py
+++ maxminddb/types.py
@@ -1,14 +1,18 @@
 """Types representing database records."""
 
-from typing import AnyStr, Union
+from __future__ import annotations
 
-Primitive = Union[AnyStr, bool, float, int]
-Record = Union[Primitive, "RecordList", "RecordDict"]
+from typing import AnyStr, TypeAlias
+
+Primitive: TypeAlias = AnyStr | bool | float | int
 
 
-class RecordList(list[Record]):
+class RecordList(list["Record"]):
     """RecordList is a type for lists in a database record."""
 
 
-class RecordDict(dict[str, Record]):
+class RecordDict(dict[str, "Record"]):
     """RecordDict is a type for dicts in a database record."""
+
+
+Record: TypeAlias = Primitive | RecordList | RecordDict
